b4eb66a8e8f852f8d718f8e759b118458f7b3f85
unknown <larissa_dantier@hotmail.com>
2021-01-18 21:00:44 -0300

    Atribuição e Ternário

---
+++ Atribuição-E-Ternário/script.js
@@ -0,0 +1,11 @@
+/* Operadores de atribuição */
+var x = 5;
+var y = 10;
+
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **=y; // x = x ** y (976525)
+
