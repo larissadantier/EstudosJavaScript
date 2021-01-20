bda8ee4532f3cb36ccc0617ce3f3fa1ee23401c4
unknown <larissa_dantier@hotmail.com>
2021-01-20 03:49:16 -0300

    Exercicio - Atribuição e Ternário

--- Atribuição-E-Ternário/script.js
+++ Atribuição-E-Ternário/script.js
@@ -9,3 +9,19 @@ x /= y; // x = x / y (0.5)
 x %= y; // x = x % y (0)
 x **=y; // x = x ** y (976525)
 
+/* Operador Ternário */
+//Abreviação de condicionais com if e else
+
+var idade= 19;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber)
+/* Geralmente utilizado quando precisamos
+atribuir um valor para uma variável, dependendo
+de uma condição */
+
+/* If abreviado */
+var possuiFaculdade = true;
+if(possuiFaculdade)
+    console.log('Possui faculdade');
+else
+    console.log('Não possui faculdade');
