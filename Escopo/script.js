0e979ecf473fe40a106d49a69462b6adf9016e6c
unknown <larissa_dantier@hotmail.com>
2021-01-28 20:46:38 -0300

    Escopo - For Loop

--- Escopo/script.js
+++ Escopo/script.js
@@ -10,8 +10,7 @@ function mostrarCarro(){
 
 mostrarCarro(); // Fusca
 //console.log(carro); //Erro, carro is not defined
-
-/* Escopo evita o conflito entre nomes */
+//Escopo evita o conflito entre nomes
 
 /* Variável Global (Erro) */
 /* Declarar variáveis sem a palavra
@@ -38,4 +37,62 @@ function mostrarCarro3(){
 }
 
 mostrarCarro3() // Meu carro é um Fusca
-console.log(carro); // Fusca
+console.log(carro3); // Fusca
+
+/* Escopo de Bloco */
+/* Variaveís criadas com var, vazam o bloco.
+Por isos com a introdução ES6 a melhor forma
+de declaramos uma variável é utilizando
+const e let, pois estas respeitam o
+escopo de bloco */
+
+if(true){
+    var carro4 = 'Fusca';
+    console.log(carro4);
+}
+console.log(carro4); // carro
+
+/* Var vaza o bloco */
+
+/* Mesmo com a condição falsa, a variável
+ainda será declarada utilizando hoisting
+e o valor ficará como indefined. */
+
+if(false) {
+    var carro5 = 'Fusca';
+    console.log(carro5);
+}
+
+console.log(carro5); // Undefined
+
+/* Const e Let no lugar de Var */
+/* Utilizar apenas const e let
+para declarar variáveis */
+
+if(true){
+    const mes = 'Dezembro';
+    console.log(mes);
+}
+
+ //console.log(mes); // Erro, carro is not defined
+
+ /* {} criam um escopo de bloco, não
+ confundir com a criação de objetos = {} */
+
+ {
+     var carro6 = 'Fusca';
+     const ano = 2018;
+ }
+
+ console.log(carro6); // Fusca
+ //console.log(ano); // erro ano is not defined
+
+ /* For Loop */
+ /* Ao utilizar var dentro de um for loop,
+ que é um bloco, o valor da variável
+ utilizada irá vazar e existir fora do loop. */
+ for(var i=0; i < 10; i++){
+     console.log(`Número ${i}`)
+ }
+
+ console.log(i); // 10
